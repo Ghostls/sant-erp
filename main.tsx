51b7e60a0c8c45bb30ddb7ebd62636a1f2fde273
Lualdo Sciscioli <Scisciolilualdo@gmail.com>
2026-05-13 16:59:12 -0400

    sys: bump version to 1.0.1 and force tsc remova

--- main.tsx
+++ main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import SantERPDashboard from './src/SantERPDashboard';
+import SantERPDashboard from './src/SantERPDashboard'; // <--- Punto de atención
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
